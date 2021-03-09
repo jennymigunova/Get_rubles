10907a29ac20752d113338e55e1e64bd5cdeeedf
Evgeniya Migunova <8880305@gmail.com>
2021-03-09 18:09:14 -0500

    Add bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int balance = 100;
+        int payment = 1100;
+        boolean hasBonus = payment > 1000;
 
+        if (hasBonus) {
+            int bonus = payment / 100;
+            int total = balance + payment + bonus;
+            System.out.println("Бонус: " + bonus + " \nИтоговая сумма на счету клиента: " + total);
+
+        } else {
+            int total = balance + payment;
+            System.out.println("Бонус: 0" + " \nИтоговая сумма на счету клиента: " + total);
+        }
     }
 }
